f8df70d0b836bad4667101073aadcb3b5cc27417
Cenk Ozan Kahraman <cokahraman@hotmail.com>
2017-10-10 14:01:35 +0100

    Fixed planning delete problem

--- client/app/planning/planning.component.ts
+++ client/app/planning/planning.component.ts
@@ -147,6 +147,7 @@ export class PlanningComponent implements OnInit {
       );
     this.dialogVisible = false;
     this.refreshData();
+    this.cd.detectChanges();
   }
 
   // findEventIndexById(id: string) {
